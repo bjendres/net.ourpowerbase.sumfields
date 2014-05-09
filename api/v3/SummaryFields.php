554b1e438a807ab9902248614380ec72e330102e
Björn Endres <endres@systopia.de>
2014-05-09 13:26:49 +0200

    more test API

--- api/v3/SummaryFields.php
+++ api/v3/SummaryFields.php
@@ -4,9 +4,19 @@
  */
 function civicrm_api3_summary_fields_rebuild($params) {
 	error_log("net.ourpowerbase.sumfields: starting data rebuild");
-	sumfields_generate_data_based_on_current_data();
+	sumfields_generate_data_based_on_current_data($session = NULL);
 	error_log("net.ourpowerbase.sumfields: enable triggers");
 	CRM_Core_DAO::triggerRebuild();
 	error_log("net.ourpowerbase.sumfields: done data rebuild");
+	return civicrm_api3_create_success();
+}
+
+/**
+ * Rebuild the summary fields
+ */
+function civicrm_api3_summary_fields_triggers($params) {
+	error_log("net.ourpowerbase.sumfields: enable triggers");
+	CRM_Core_DAO::triggerRebuild();
+	error_log("net.ourpowerbase.sumfields: done");
 	return civicrm_api3_create_success();
 }
