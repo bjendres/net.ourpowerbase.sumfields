7fa1c855e8164892f15be0fb2c07d00e48468d2d
Björn Endres <endres@systopia.de>
2014-05-14 10:22:47 +0200

    detached triggers from rebuild

--- api/v3/SummaryFields.php
+++ api/v3/SummaryFields.php
@@ -5,8 +5,6 @@
 function civicrm_api3_summary_fields_rebuild($params) {
 	error_log("net.ourpowerbase.sumfields: starting data rebuild");
 	sumfields_generate_data_based_on_current_data($session = NULL);
-	error_log("net.ourpowerbase.sumfields: enable triggers");
-	CRM_Core_DAO::triggerRebuild();
 	error_log("net.ourpowerbase.sumfields: done data rebuild");
 	return civicrm_api3_create_success();
 }
